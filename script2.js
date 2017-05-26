20e385350e5fc0078412f7d8cba7b7ba1e86e9f9
David A Koch <kodave1@gmail.com>
2017-05-25 20:21:47 -0400

    added document on ready

--- script2.js
+++ script2.js
@@ -7,83 +7,85 @@
 
 
 	//keep track on who is playing
+$(document).ready(function(){
 
-	function startGame(){
+		function startGame(){
 
-		for(var i = 1; i<=9; i++){
-			clearBox(i);
+			for(var i = 1; i<=9; i++){
+				clearBox(i);
+			}
+
+			document.turn = "X";
+			if (Math.random() < 0.5){
+				document.turn = "O";
+			} 
+			document.winner = null;
+
+			setMessage(document.turn + " will start.");
+
 		}
 
-		document.turn = "X";
-		if (Math.random() < 0.5){
-			document.turn = "O";
-		} 
-		document.winner = null;
+		//function for setting message
 
-		setMessage(document.turn + " will start.");
-
-	}
-
-	//function for setting message
-
-	function setMessage(msg){
-		//this definitely takes a jquery function...maybe value or html
-		document.getElementById("message").innerText = msg;
-	}
-
-	//i think you can use jquery toggle for this...
-	function nextMove(square){
-		if(document.winner != null){
-			setMessage(document.winner + " already won the game.");
-		} else if(square.innerText == ""){
-			square.innerText = document.turn;
-			switchTurn();
-		} else {
-			setMessage("That Square is Already Used.")
+		function setMessage(msg){
+			//this definitely takes a jquery function...maybe value or html
+			document.getElementById("message").innerText = msg;
 		}
-	}	
 
-	function switchTurn(){
-		if(checkForWinner(document.turn)) {
-			setMessage("Congratulations, " + document.turn + "! You've won!")
-			document.winner = document.turn;
-		} else if(document.turn == "X"){
-			document.turn = "O"; 
-			setMessage("It's " + document.turn + "'s turn");
-		} else {
-			document.turn = "X"; 
-			setMessage("It's " + document.turn + "'s turn");
+		//i think you can use jquery toggle for this...
+		function nextMove(square){
+			if(document.winner != null){
+				setMessage(document.winner + " already won the game.");
+			} else if(square.innerText == ""){
+				square.innerText = document.turn;
+				switchTurn();
+			} else {
+				setMessage("That Square is Already Used.")
+			}
+		}	
+
+		function switchTurn(){
+			if(checkForWinner(document.turn)) {
+				setMessage("Congratulations, " + document.turn + "! You've won!")
+				document.winner = document.turn;
+			} else if(document.turn == "X"){
+				document.turn = "O"; 
+				setMessage("It's " + document.turn + "'s turn");
+			} else {
+				document.turn = "X"; 
+				setMessage("It's " + document.turn + "'s turn");
+			}
 		}
-	}
 
-	function checkForWinner(move) {
-		var result = false;
-		if(checkRow(1, 2, 3, move) || 
-		   checkRow(4, 5, 6, move) || 
-		   checkRow(7, 8, 9, move) || 
-		   checkRow(1, 4, 7, move) || 
-		   checkRow(2, 5, 8, move) || 
-		   checkRow(3, 6, 9, move) ||
-		   checkRow(1, 5, 9, move) || 
-		   checkRow(3, 5, 7, move)){
-			result = true;
+		function checkForWinner(move) {
+			var result = false;
+			if(checkRow(1, 2, 3, move) || 
+			   checkRow(4, 5, 6, move) || 
+			   checkRow(7, 8, 9, move) || 
+			   checkRow(1, 4, 7, move) || 
+			   checkRow(2, 5, 8, move) || 
+			   checkRow(3, 6, 9, move) ||
+			   checkRow(1, 5, 9, move) || 
+			   checkRow(3, 5, 7, move)){
+				result = true;
+			}
+			return result;
 		}
-		return result;
-	}
 
-	function checkRow(a, b, c, move){
-		var result = false;
-		if(getBox(a) == move && getBox(b) == move && getBox(c) == move) {
-			result = true
+		function checkRow(a, b, c, move){
+			var result = false;
+			if(getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+				result = true
+			}
+			return result;
 		}
-		return result;
-	}
 
-	function getBox(number){
-		//definitely a jquery method such as val or html
-		return document.getElementById("s" + number).innerText;
-	}
+		function getBox(number){
+			//definitely a jquery method such as val or html
+			return document.getElementById("s" + number).innerText;
+		}
 
-	function clearBox(number){
-		document.getElementById("s" + number).innerText = "";
-	}
+		function clearBox(number){
+			document.getElementById("s" + number).innerText = "";
+		}
+});
